2bbbc5e0c5c434a1814bb0371a57c6533e345d35
ana <anameciarobalo@gmail.com>
2020-02-16 14:30:53 +0000

    fixes bug: when validating user, if user was true, the validate method wasn't sending a token back to the client

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     def validate
         user = get_current_user
         if user
-            render json: user #render something for that user
+            render json: { user:  user, token: issue_token({ id: user.id }) }
         else
           render json: { error: 'You are not authorized.' }, status: 401
         end
