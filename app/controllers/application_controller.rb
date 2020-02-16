7a4a1479ddea68f5463311a484ec87133ab44220
ana <anameciarobalo@gmail.com>
2020-02-16 10:05:17 +0000

    adds authorization to sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+    def get_current_user
+        id = decoded_token['id']
+        User.find_by(id: id)
+    end
+    
+    def decoded_token
+        begin
+            JWT.decode(token, secret).first #return de users id {id: 1}
+        rescue
+            {}
+        end
+    end 
+
+    def token 
+        request.headers['Authorization']
+    end
+
+    def issue_token(data)
+        JWT.encode(data, secret)
+    end
+
+    def secret
+        ENV['SECRET']
+    end 
 end
--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,16 @@ Bundler.require(*Rails.groups)
 
 module Mod5ProjectBackEnd
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'env_var.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
